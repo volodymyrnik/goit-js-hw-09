5d87956571e57314601f97c25eebe9746ad54513
Volodymyr Nikiforenko <vl.nikiforenko@gmail.com>
2025-02-01 01:02:08 +0200

    Fix deployment

--- vite.config.js
+++ vite.config.js
@@ -34,7 +34,7 @@ export default defineConfig(({ command }) => {
           },
         },
       },
-      outDir: '../dist',
+      outDir: 'dist',
       emptyOutDir: true,
     },
     plugins: [
